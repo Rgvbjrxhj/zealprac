8521fe0ae49f00514f37395f60c7115eaf6d9ae2
unknown <rushikeshuttekar535@gmail.com>
2024-10-17 10:25:50 +0530

    new updated py

--- test.py
+++ test.py
@@ -1,60 +1,65 @@
-a =[1,2,3,]
-b=[4,5,6]
-a.extend(b)
-print(a)
+# a =[1,2,3,]
+# b=[4,5,6]
+# a.extend(b)
+# print(a)
 
-c="HeLLo UsEr"
-print(c.capitalize())
+# c="HeLLo UsEr"
+# print(c.capitalize())
 
-a={
-    "car":"porche",
-    "model":"x",
-    "year":2020
-}
+# a={
+#     "car":"porche",
+#     "model":"x",
+#     "year":2020
+# }
 
-for x,y in (a.items()):
-    print(x)
-    print(y)
+# for x,y in (a.items()):
+#     print(x)
+#     print(y)
 
-a={
-    "car":"porche",
-    "model":"x",
-    "year":2020
-}
+# a={
+#     "car":"porche",
+#     "model":"x",
+#     "year":2020
+# }
 
-for x in (a):
-    print(x)
-    print(a[x])
+# for x in (a):
+#     print(x)
+#     print(a[x])
 
-a={
-    "T213055":{
-        "name":"Rushikesh",
-        "class":"TE"
-    },
-    "T213068":{
-        "name":"krishna",
-        "class":"Te"
-    },
-    "T213011":{
-        "name":"janhavi",
-        "class":"TE"
-    }
-}
-print(a)
+# a={
+#     "T213055":{
+#         "name":"Rushikesh",
+#         "class":"TE"
+#     },
+#     "T213068":{
+#         "name":"krishna",
+#         "class":"Te"
+#     },
+#     "T213011":{
+#         "name":"janhavi",
+#         "class":"TE"
+#     }
+# }
+# print(a)
 
-roll = int(input("Enter roll no: "))
+# roll = int(input("Enter roll no: "))
 
-if roll<=10:
-    print("Belongs to A division")
-elif roll>10 and roll<=20:
-    print("Belongs to B divion")
-elif roll>20 and roll<=30:
-    print("Belong to C divsion")
-else:
-    print("Invalid roll no")
+# if roll<=10:
+#     print("Belongs to A division")
+# elif roll>10 and roll<=20:
+#     print("Belongs to B divion")
+# elif roll>20 and roll<=30:
+#     print("Belong to C divsion")
+# else:
+#     print("Invalid roll no")
 
-a=5
-i=0
-while a-i:
+# a=5
+# i=0
+# while a-i:
+#     print(a)
+#     i=i+1
+
+a= "banana"
+for x in range (a):
     print(a)
-    i=i+1
+ 
